aa218d928486003553464e635d62f3c09448328f
ixtf <ixtf1984@gmail.com>
2026-01-15 01:53:28 +0800

    fix cqrs

--- broker/src/main/kotlin/com/github/ixtf/broker/BrokerClient.kt
+++ broker/src/main/kotlin/com/github/ixtf/broker/BrokerClient.kt
@@ -1,27 +1,11 @@
 package com.github.ixtf.broker
 
 import com.github.ixtf.broker.internal.DefaultBrokerClient
-import io.rsocket.Payload
 import io.vertx.core.Vertx
-import org.reactivestreams.Publisher
-import reactor.core.Disposable
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
-interface BrokerClient : Disposable {
-  val target: String
+interface BrokerClient : NativeClient {
 
   fun route(route: BrokerRouteOptions): BrokerRoute
-
-  fun fireAndForget(payloadMono: Mono<Payload>): Mono<Void>
-
-  fun requestResponse(payloadMono: Mono<Payload>): Mono<Payload>
-
-  fun requestStream(payloadMono: Mono<Payload>): Flux<Payload>
-
-  fun requestChannel(payloads: Publisher<Payload>): Flux<Payload>
-
-  fun metadataPush(payloadMono: Mono<Payload>): Mono<Void>
 
   companion object {
     fun create(vertx: Vertx, options: BrokerClientOptions = BrokerClientOptions()): BrokerClient =
