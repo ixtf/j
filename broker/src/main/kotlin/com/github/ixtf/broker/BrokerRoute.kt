be48df0c36799f47bd6396fb669ff205dea5e59e
ixtf <ixtf1984@gmail.com>
2026-01-15 01:33:42 +0800

    fix cqrs

--- broker/src/main/kotlin/com/github/ixtf/broker/BrokerRoute.kt
+++ broker/src/main/kotlin/com/github/ixtf/broker/BrokerRoute.kt
@@ -3,16 +3,15 @@ package com.github.ixtf.broker
 import io.cloudevents.CloudEvent
 import io.rsocket.Payload
 import kotlinx.coroutines.flow.Flow
-import reactor.core.publisher.Mono
 
 interface BrokerRoute {
   suspend fun fireAndForget(block: suspend () -> CloudEvent)
 
-  suspend fun requestResponse(block: suspend () -> CloudEvent): Mono<Payload>
+  suspend fun requestResponse(block: suspend () -> CloudEvent): Payload
 
-  suspend fun requestStream(block: suspend () -> CloudEvent): Flow<Payload>
+  fun requestStream(block: suspend () -> CloudEvent): Flow<Payload>
 
-  suspend fun requestChannel(block: () -> Flow<CloudEvent>): Flow<Payload>
+  fun requestChannel(block: () -> Flow<CloudEvent>): Flow<Payload>
 
   suspend fun metadataPush(block: suspend () -> CloudEvent)
 }
--- broker/src/main/kotlin/com/github/ixtf/broker/internal/DefaultBrokerRoute.kt
+++ broker/src/main/kotlin/com/github/ixtf/broker/internal/DefaultBrokerRoute.kt
@@ -2,32 +2,34 @@ package com.github.ixtf.broker.internal
 
 import com.github.ixtf.broker.BrokerRoute
 import com.github.ixtf.broker.RouteOptions
+import com.github.ixtf.broker.toPayload
 import io.cloudevents.CloudEvent
 import io.rsocket.Payload
 import kotlinx.coroutines.flow.Flow
-import reactor.core.publisher.Mono
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
 
 internal class DefaultBrokerRoute(
   private val brokerClient: DefaultBrokerClient,
   private val options: RouteOptions,
 ) : BrokerRoute {
   override suspend fun fireAndForget(block: suspend () -> CloudEvent) {
-    TODO("Not yet implemented")
+    brokerClient.fireAndForget(mono { block().toPayload() }).awaitSingleOrNull()
   }
 
-  override suspend fun requestResponse(block: suspend () -> CloudEvent): Mono<Payload> {
-    TODO("Not yet implemented")
-  }
+  override suspend fun requestResponse(block: suspend () -> CloudEvent): Payload =
+    brokerClient.requestResponse(mono { block().toPayload() }).awaitSingle()
 
-  override suspend fun requestStream(block: suspend () -> CloudEvent): Flow<Payload> {
-    TODO("Not yet implemented")
-  }
+  override fun requestStream(block: suspend () -> CloudEvent): Flow<Payload> =
+    brokerClient.requestResponse(mono { block().toPayload() }).asFlow()
 
-  override suspend fun requestChannel(block: () -> Flow<CloudEvent>): Flow<Payload> {
+  override fun requestChannel(block: () -> Flow<CloudEvent>): Flow<Payload> {
     TODO("Not yet implemented")
   }
 
   override suspend fun metadataPush(block: suspend () -> CloudEvent) {
-    TODO("Not yet implemented")
+    brokerClient.metadataPush(mono { block().toPayload() }).awaitSingleOrNull()
   }
 }
