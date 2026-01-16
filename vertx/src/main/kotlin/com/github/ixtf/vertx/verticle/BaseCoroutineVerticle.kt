337a6ce1c88773a41d99d846cdd3cf2495dc1c0a
ixtf <ixtf1984@gmail.com>
2026-01-17 00:07:56 +0800

    fix cqrs

--- vertx/src/main/kotlin/com/github/ixtf/vertx/verticle/BaseCoroutineVerticle.kt
+++ vertx/src/main/kotlin/com/github/ixtf/vertx/verticle/BaseCoroutineVerticle.kt
@@ -1,6 +1,8 @@
 package com.github.ixtf.vertx.verticle
 
 import cn.hutool.log.Log
+import io.vertx.core.Context
+import io.vertx.core.Vertx
 import io.vertx.core.eventbus.EventBus
 import io.vertx.core.file.FileSystem
 import io.vertx.kotlin.coroutines.CoroutineEventBusSupport
@@ -12,4 +14,11 @@ abstract class BaseCoroutineVerticle :
   protected val log: Log by lazy { Log.get(this::class.java) }
   protected val eventBus: EventBus by lazy { vertx.eventBus() }
   protected val fileSystem: FileSystem by lazy { vertx.fileSystem() }
+  protected lateinit var context: Context
+    private set
+
+  override fun init(vertx: Vertx, context: Context) {
+    super.init(vertx, context)
+    this.context = context
+  }
 }
