85a2ca5ab649b7baadd5685a8a3a7f76ed700ea2
jzb <ixtf1984@gmail.com>
2026-01-16 18:26:31 +0800

    add broker

--- broker/src/main/kotlin/com/github/ixtf/broker/kit/PayloadKit.kt
+++ broker/src/main/kotlin/com/github/ixtf/broker/kit/PayloadKit.kt
@@ -9,6 +9,7 @@ import io.netty.buffer.ByteBufUtil
 import io.netty.util.ReferenceCountUtil
 import io.rsocket.Payload
 import io.vertx.core.buffer.Buffer
+import io.vertx.core.internal.buffer.BufferInternal
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import java.nio.charset.StandardCharsets
@@ -24,6 +25,7 @@ inline fun <reified T> Payload.readValue(): T = requireNotNull(readValueOrNull()
 
 inline fun <reified T> Payload.readValueOrNull(): T? = data().readValueOrNull()
 
+/** 支持重复读取的解析工具 使用 [slice] 确保不移动原始 ByteBuf 的 readerIndex */
 inline fun <reified T> ByteBuf.readValueOrNull(): T? =
   if (readableBytes() <= 0) null
   else
@@ -31,9 +33,9 @@ inline fun <reified T> ByteBuf.readValueOrNull(): T? =
       CloudEvent::class -> CLOUD_EVENT_FORMAT.deserialize(ByteBufUtil.getBytes(this))
       String::class -> toString(StandardCharsets.UTF_8)
       ByteArray::class -> ByteBufUtil.getBytes(this)
-      Buffer::class -> Buffer.buffer(ByteBufUtil.getBytes(this))
-      JsonObject::class -> Buffer.buffer(ByteBufUtil.getBytes(this)).toJsonObject()
-      JsonArray::class -> Buffer.buffer(ByteBufUtil.getBytes(this)).toJsonArray()
+      Buffer::class -> BufferInternal.buffer(slice())
+      JsonObject::class -> BufferInternal.buffer(slice()).toJsonObject()
+      JsonArray::class -> BufferInternal.buffer(slice()).toJsonArray()
       else -> ByteBufInputStream(this).use { MAPPER.readValue<T>(it) }
     }
       as T
